efd29f242c09ed75afa51fd55162c35b41f95834
A2 <>
2018-09-29 13:01:26 +0300

    test

--- SQL/SQLPlus01.sql
+++ SQL/SQLPlus01.sql
@@ -19,3 +19,6 @@ SQL> WITH t AS (SELECT '5346743912984639454262436' a FROM DUAL) SELECT 9 * (LENG
                                                                              119
 
 SQL>
+;
+select * from DEPARTMENT;
+
