1c49cdcadfac6366ff9cf370ad02c2cbd1624775
A2 <>
2018-09-29 13:32:50 +0300

    add test

--- SQL/XE01.sql
+++ SQL/XE01.sql
@@ -6,3 +6,15 @@ select * from SYS.department t1,
            FROM SYS.employee
            GROUP BY department_id
          ) t2;
+         
+         
+  CREATE TABLE "DEPARTMENT" 
+   (	"ID" NUMBER, 
+	"NAME" VARCHAR2(100 BYTE));       
+    
+    
+select * from department t1,
+         ( SELECT department_id, COUNT(*) AS staff
+           FROM employee
+           GROUP BY department_id
+         ) t2;    
